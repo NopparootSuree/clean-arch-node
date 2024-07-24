07e7e3cda5c9a400ab0702b6703dff52520b5f75
Snc-nopparoot <nopparoot@sncformer.com>
2024-07-24 16:48:41 +0700

    add interface user

---
+++ src/domain/repositories/user/UserRepository.ts
@@ -0,0 +1,12 @@
+import { User } from '@domain/entities/user/User';
+import { Transaction } from '@infrastructure/database/Transaction';
+import { PaginatedResult } from '@application/use-cases/material/FindMaterialsUseCase';
+
+export interface UserRepository {
+  findAll(options: { page: number; limit: number }): Promise<PaginatedResult<User>>;
+  count(): Promise<number>;
+  findById(id: number): Promise<User | null>;
+  create(User: User, transaction?: Transaction): Promise<User>;
+  update(User: User, transaction?: Transaction): Promise<User>;
+  delete(User: User, transaction?: Transaction): Promise<User>;
+}
