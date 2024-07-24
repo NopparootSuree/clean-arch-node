88a50691fbb0230d09e87625dc9c1fc1e03efb25
Snc-nopparoot <nopparoot@sncformer.com>
2024-07-24 16:27:48 +0700

    init new branch users

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,5 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   plugins: ['@typescript-eslint', 'prettier'],
-  rules: { 'prettier/prettier': 'error' },
+  rules: { 'prettier/prettier': 0 },
 };
